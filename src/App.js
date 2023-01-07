734404ef3675da1f3e6d3a3c4528c76f7b45654b
Aishu2604 <aishwary.nigam04@gmail.com>
2023-01-07 16:19:48 +0530

    arramton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import TimeLogger from './TimeLogger';
+import "./App.css";
+import Component from "./Component";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <TimeLogger />
-  )
+    <div>
+      <h2>Welcome....</h2>
+      <Switch>
+        <Route path="/list">
+          <Component />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
